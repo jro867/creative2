d0bac53ea1acd4f7394c8253fe789f264c0114a6
Jesus Ramos <jro867@gmail.com>
2016-09-26 07:35:38 -0600

    some bugs fixed

--- script.js
+++ script.js
@@ -1,63 +1,64 @@
 
+
+
+
+    var app = angular.module('jobBumble',[]);
+	app.controller('searchController', function($scope){
+	$scope.jobList = [];
+	var url = "https://api.usa.gov/jobs/search.json?";
+	
+	//query term into url
+	// var queryString = famText:$scope.jobQuery;
+	var parseQueryString = function(queryString){
+		var params = {}, queries, temp, i, l;
+		// Split into key/value pairs
+		queries = queryString.split("&amp;");
+		// Convert the array of strings into an object
+		for ( i = 0, l = queries.length; i < l; i++ ) {
+			temp = queries[i].split('=');
+			params[temp[0]] = temp[1];
+		}
+		console.log('Params: ' + params);
+		return params;
+	};
+	url+=parseQueryString;
+	//get state and append
+	if ($scope.state.value != null) {
+		url+='+in+'+scope.state.value;
+		console.log(url);
+	}
+	$scope.searchCareers = function() {
+		//search jobs and get info
+		$.ajax({
+			url: myurl,
+			dataType: "json",
+			success: function(parsed_json){
+				$.each(parsed_json['items'],function(name2,values) {
+					var link=values.url;
+					var position=values.position_title;
+					var org=values.organization_name;
+					var avg_salary=(values.minimum + values.maximum)/2;
+					var location=values.locations[0];
+					//populate into jobList array
+					// $scope.jobList.push({$scope.link,$scope.position,$scope.org,$scope.avg_salary,$scope.location});
+				});
+			}
+		});
+	console.log(jobList[0]);
+	};
+});
+
+
 $(document).ready(function(){
 
 /** ===== TOGGLES SEARCH FUNCTIONALITY ==== **/
 
  $(".search-button").click(function(){
  	console.log("clicking");
-  $("#search-container").toggle( "fast", function(){
+	  $("#search-container").toggle("fast", function(){
 
+	  });
   });
-});
-
-
-
-//     var app = angular.module('jobBumble', []);
-// 	app.controller('searchController', function($scope){
-// 	$scope.jobList = [];
-// 	var url = "https://api.usa.gov/jobs/search.json?";
-// 	//query term into url
-// 	// var queryString = famText:$scope.jobQuery;
-// 	var parseQueryString = function( queryString ) {
-// 		var params = {}, queries, temp, i, l;
-// 		// Split into key/value pairs
-// 		queries = queryString.split("&amp;");
-// 		// Convert the array of strings into an object
-// 		for ( i = 0, l = queries.length; i < l; i++ ) {
-// 			temp = queries[i].split('=');
-// 			params[temp[0]] = temp[1];
-// 		}
-// 		console.log('Params: ' + params);
-// 		return params;
-// 	};
-// 	url+=parseQueryString;
-// 	//get state and append
-// 	if ($scope.state.value !null) {
-// 		url+='+in+'+scope.state.value;
-// 		console.log(url);
-// 	}
-// 	$scope.searchCareers = function() {
-// 		//search jobs and get info
-// 		$.ajax({
-// 			url: myurl,
-// 			dataType: "json",
-// 			success: function(parsed_json){
-// 				$.each(parsed_json['items'],function(name2,values) {
-// 					var link=values.url;
-// 					var position=values.position_title;
-// 					var org=values.organization_name;
-// 					var avg_salary=(values.minimum + values.maximum)/2;
-// 					var location=values.locations[0];
-// 					//populate into jobList array
-// 					$scope.jobList.push({$scope.link,$scope.position,$scope.org,$scope.avg_salary,$scope.location});
-// 				});
-// 			}
-// 		});
-// 	console.log(jobList[0]);
-// 	};
-// });
-
-
 });
 
 
