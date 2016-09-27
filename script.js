8392f919c193d2e5a25cad374ebd6db3b1df6123
Jesus Ramos <jro867@gmail.com>
2016-09-26 19:47:25 -0600

    getting nursing

--- script.js
+++ script.js
@@ -8,44 +8,46 @@
 	var url = "https://api.usa.gov/jobs/search.json?";
 	
 	//query term into url
-	// var queryString = famText:$scope.jobQuery;
-	var parseQueryString = function(queryString){
-		var params = {}, queries, temp, i, l;
-		// Split into key/value pairs
-		queries = queryString.split("&amp;");
-		// Convert the array of strings into an object
-		for ( i = 0, l = queries.length; i < l; i++ ) {
-			temp = queries[i].split('=');
-			params[temp[0]] = temp[1];
-		}
-		console.log('Params: ' + params);
-		return params;
-	};
-	url+=parseQueryString;
-	//get state and append
-	if ($scope.state.value != null) {
-		url+='+in+'+scope.state.value;
-		console.log(url);
-	}
+
+	// var parseQueryString = function(queryString){
+	// 	var params = {}, queries, temp, i, l;
+	// 	// Split into key/value pairs
+	// 	queries = queryString.split("&amp;");
+	// 	// Convert the array of strings into an object
+	// 	for ( i = 0, l = queries.length; i < l; i++ ) {
+	// 		temp = queries[i].split('=');
+	// 		params[temp[0]] = temp[1];
+	// 	}
+	// 	console.log('Params: ' + params);
+	// 	return params;
+	// };
+	// url+=parseQueryString;
+	// //get state and append
+	
+	// url+='+in+'+ $scope.state.find("option:selected").text();
+	// console.log(url);
+	
 	$scope.searchCareers = function() {
-		//search jobs and get info
-		$.ajax({
-			url: myurl,
-			dataType: "json",
-			success: function(parsed_json){
-				$.each(parsed_json['items'],function(name2,values) {
-					var link=values.url;
-					var position=values.position_title;
-					var org=values.organization_name;
-					var avg_salary=(values.minimum + values.maximum)/2;
-					var location=values.locations[0];
-					//populate into jobList array
-					// $scope.jobList.push({$scope.link,$scope.position,$scope.org,$scope.avg_salary,$scope.location});
-				});
+			var queryString = $scope.jobQuery;
+	console.log("queryString: " + queryString);
+	// 	//search jobs and get info
+	// 	$.ajax({
+	// 		url: myurl,
+	// 		dataType: "json",
+	// 		success: function(parsed_json){
+	// 			$.each(parsed_json['items'],function(name2,values) {
+	// 				var link=values.url;
+	// 				var position=values.position_title;
+	// 				var org=values.organization_name;
+	// 				var avg_salary=(values.minimum + values.maximum)/2;
+	// 				var location=values.locations[0];
+	// 				//populate into jobList array
+	// 				// $scope.jobList.push({$scope.link,$scope.position,$scope.org,$scope.avg_salary,$scope.location});
+	// 			});
 			}
-		});
-	console.log(jobList[0]);
-	};
+		// });
+	// console.log(jobList[0]);
+	// };
 });
 
 
